322b21ed488ebbd9eef675a0d685b04d93a31853
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-09-19 14:31:28 +0200

    fix(async): preserve sparse plugin error message

--- plugins/async.js
+++ plugins/async.js
@@ -43,7 +43,7 @@ function requestedTypes(source) {
   if (Array.isArray(source)) {
     const types = []
     for (let index = 0; index < source.length; index++) {
-      if (!Object.hasOwn(source, index)) throw new RangeError('Unknown editor preset type: undefined')
+      if (!Object.hasOwn(source, index)) throw new RangeError('Unknown editor block plugin type: undefined')
       types.push(source[index])
     }
     return [...new Set(types)]
